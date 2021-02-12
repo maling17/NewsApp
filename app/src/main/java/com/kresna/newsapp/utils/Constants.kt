a59c67db04f658108e9347b0b90baf8ee8cb6cc1
Kresna <kresnaa09@yahoo.com>
2021-02-12 20:21:38 +0700

    update

--- app/src/main/java/com/kresna/newsapp/utils/Constants.kt
+++ app/src/main/java/com/kresna/newsapp/utils/Constants.kt
@@ -1,8 +1,8 @@
 package com.kresna.newsapp.utils
 
 class Constants {
-    companion object{
-        const val API_KEY = "64c4f38212654d698be93e1d502aa97b"
+    companion object {
+        const val API_KEY = "API KEY"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
